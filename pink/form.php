df90b28dff36bdf0c5f04056b4a79c124de7820f
Lavanda217 <lavanderwerdi@gmail.com>
2019-07-27 16:09:38 -0600

    lkjm

--- pink/form.php
+++ pink/form.php
@@ -6,7 +6,7 @@
   $subject = "REGISTRATION https://lavanda217.github.io/pink/index.html";
   $msg = "YOUR INFORMATION:\n" ."NAME: $name\n" ."newsletter: $newsletter\n" ."YOUR email: $email\n" ."COUNTRY: $country";
   $headers = "Content-type: text/plain; charset=UTF-8" . "\r\n";
-  $headers .= "From: Your nam,e <email>" . "\r\n";
+  $headers .= "From: Lavander <lavanderwerdi@gmail.com>" . "\r\n";
   $headers .= "Bcc: email". "\r\n";
   if(!empty($name) && !empty($newsletter) && !empty($email) && !empty($country) && filter_var($email, FILTER_VALIDATE_EMAIL)){
     mail($email, $subject, $msg, $headers);
