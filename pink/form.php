4c973a93dcd0a7f0c16372ab9fca6d04143b097b
Lavanda217 <lavanderwerdi@gmail.com>
2019-07-27 16:06:15 -0600

    hj5y3n

--- pink/form.php
+++ pink/form.php
@@ -1,15 +1,15 @@
 <?php
   $name = trim(strip_tags($_POST["name"]));
-  $sex = trim(strip_tags($_POST["sex"]));
+  $sex = trim(strip_tags($_POST["newsletter"]));
   $email = trim(strip_tags($_POST["email"]));
   $country = trim(strip_tags($_POST["country"]));
-  $subject = "Регистрация на сайте url_вашего_сайта";
-  $msg = "Ваши данные формы регистрации:\n" ."Имя: $name\n" ."Пол: $sex\n" ."Ваш email: $email\n" ."Страна: $country";
+  $subject = "REGISTRATION https://lavanda217.github.io/pink/index.html";
+  $msg = "YOUR INFORMATION:\n" ."NAME: $name\n" ."newsletter: $newsletter\n" ."YOUR email: $email\n" ."COUNTRY: $country";
   $headers = "Content-type: text/plain; charset=UTF-8" . "\r\n";
-  $headers .= "From: Ваше_имя <ваш_email>" . "\r\n";
-  $headers .= "Bcc: ваш_email". "\r\n";
-  if(!empty($name) && !empty($sex) && !empty($email) && !empty($country) && filter_var($email, FILTER_VALIDATE_EMAIL)){
+  $headers .= "From: Your nam,e <email>" . "\r\n";
+  $headers .= "Bcc: email". "\r\n";
+  if(!empty($name) && !empty($newsletter) && !empty($email) && !empty($country) && filter_var($email, FILTER_VALIDATE_EMAIL)){
     mail($email, $subject, $msg, $headers);
-    echo "Спасибо! Вы успешно зарегистрировались.";
+    echo "Thank you, my friend.";
     }
 ?>
